ad38b0ea594809fabc28ab4ec1ea1589c73a4a91
Baumendhra <baumendhra@gmail.com>
2025-04-23 00:26:16 +0530

    updating

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import BusList from './components/BusList';
+import SeatLayout from './components/SeatLayout';
+import Booking from './components/Booking';
+import Login from './components/Login';
+import Register from './components/Register';
+import Loading from './components/Loading';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/bus-list" element={<BusList />} />
+        <Route path="/seat-layout" element={<SeatLayout />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/loading" element={<Loading />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/BusList.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from "react";
+
+const BusList = () => {
+    const [buses, setBuses] = useState([]);
+}
